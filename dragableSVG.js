5eddbd3abac1c2bb4f1a5ce77720dfc30a1c92f3
EwanP0208 <33698412+EwanP0208@users.noreply.github.com>
2021-12-08 18:20:08 +0000

    Added snapping, bounds and grid lines

--- dragableSVG.js
+++ dragableSVG.js
@@ -1,13 +1,28 @@
+const SVGNS = "http://www.w3.org/2000/svg"
+const CELL_WIDTH = 0.5;
+
 //Followed guide at https://www.petercollingridge.co.uk/tutorials/svg/interactive/dragging/
 function makeDraggable(evt) {
     const SVG = evt.target;
     let selectedElement, offset, transform;
 
-    //Boundary to confine any elements with the confine class to
-    let boundaryX1 = 10.5;
-    let boundaryX2 = 30;
-    let boundaryY1 = 2.2;
-    let boundaryY2 = 19.2;
+    //Used with element size to fix dragging to boundary
+    let elementBounds = {};
+
+    //Boundary to confine all elements to
+    let boundary = {
+        x1: 1, x2: 29,
+        y1: 1, y2: 19
+    }
+
+    //Add the boundary element
+    const boundaryElement = createBoundaryElement(boundary);
+
+    //First child will be the background
+    SVG.insertBefore(boundaryElement, SVG.children[1]);
+
+    //Add grid lines that elements will snap to
+    addGridLines(SVG, CELL_WIDTH);
 
     //Bindings for mouse
     SVG.addEventListener('mousedown', startDrag);
@@ -47,6 +62,10 @@ function makeDraggable(evt) {
                 selectedElement.transform.baseVal.insertItemBefore(translate, 0);
             }
 
+            //Calculate the possible coordinates for the element in the boundary
+            let bbox = selectedElement.getBBox();
+            elementBounds = calculateBounds(bbox, boundary)
+
             //Get the starting translation amount
             transform = transforms.getItem(0);
 
@@ -63,9 +82,18 @@ function makeDraggable(evt) {
             evt.preventDefault();
 
             let position = getMousePosition(evt);
+
+            //Rounded to the nearest 0.5
+            let dx = roundHalf(position.x - offset.x);
+            let dy = roundHalf(position.y - offset.y);
             
+            if (dx < elementBounds.minX) { dx = elementBounds.minX; }
+            if (dx > elementBounds.maxX) { dx = elementBounds.maxX; }
+            if (dy < elementBounds.minY) { dy = elementBounds.minY; }
+            if (dy > elementBounds.maxY) { dy = elementBounds.maxY; }
+
             //Updated the selected transform position minus the offset
-            transform.setTranslate(position.x - offset.x, position.y - offset.y)
+            transform.setTranslate(dx, dy)
         }
     }
 
@@ -90,4 +118,77 @@ function makeDraggable(evt) {
             y: (evt.clientY - CTM.f) / CTM.d
         }
     }
+}
+
+function roundHalf(num) {
+    const multiplier = 1 / CELL_WIDTH;
+    return Math.round(num * multiplier) / multiplier
+}
+
+function calculateBounds(bbox, boundary) {
+    return {
+        minX: boundary.x1 - bbox.x,
+        maxX: boundary.x2 - bbox.x - bbox.width,
+        minY: boundary.y1 - bbox.y,
+        maxY: boundary.y2 - bbox.y - bbox.height
+    }
+}
+
+function createBoundaryElement(boundary) {
+    const offsetAmount = CELL_WIDTH / 2;
+    const {x1, x2, y1, y2} = offsetBoundaryPath(boundary, offsetAmount)
+
+    //Start TL, go to TR, then BR, then BL and back to TL
+    const pathCommand = `M${x1} ${y1} H${x2} V${y2} H${x1} Z`
+
+    let newPath = document.createElementNS(SVGNS, "path");
+    newPath.setAttribute("d", pathCommand);
+    newPath.setAttribute("stroke", "black");
+    newPath.setAttribute("stroke-width", CELL_WIDTH);
+    newPath.setAttribute("fill", "none");
+
+    return newPath;
+}
+
+function offsetBoundaryPath({x1, x2, y1, y2}, offsetAmount) {
+    return {
+        x1: x1 - offsetAmount,
+        y1: y1 - offsetAmount,
+        x2: x2 + offsetAmount,
+        y2: y2 + offsetAmount
+    }
+}
+
+function addGridLines(svg, cellSize) {
+    const newGridLine = (start, direction, length) => {
+        let pathCommand = "";
+
+        if (direction === "horizontal") {
+            pathCommand = `M0 ${start} L${length} ${start}`
+        }
+
+        if (direction === "vertical") {
+            pathCommand = `M${start} 0 L${start} ${length}`
+        }
+
+        let newPath = document.createElementNS(SVGNS, "path");
+        newPath.setAttribute("d", pathCommand);
+        newPath.setAttribute("stroke", "#BBB");
+        newPath.setAttribute("stroke-width", 0.0075);
+
+        return newPath;
+    }
+    
+    const { width, height } = svg.viewBox.baseVal;
+    let gridGroup = document.createElementNS(SVGNS, "g");
+
+    for (let i = cellSize; i < width; i += cellSize) {
+        gridGroup.appendChild(newGridLine(i, "vertical", height));
+    }
+
+    for (let j = cellSize; j < height; j += cellSize) {
+        gridGroup.appendChild(newGridLine(j, "horizontal", width));
+    }
+
+    svg.insertBefore(gridGroup, svg.children[1]);
 }
